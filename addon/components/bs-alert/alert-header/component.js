8695e55652d6be66380cbe5b9230d07b21e32bb9
Brandon Drake <tomogiusuru@gmail.com>
2017-11-16 14:31:59 -0700

    removed unneeded junk

--- addon/components/bs-alert/alert-header/component.js
+++ addon/components/bs-alert/alert-header/component.js
@@ -7,17 +7,6 @@ const AlertHeader = Component.extend(BaseMixin, {
   tagName: 'h4',
 
   base: 'alert-header',
-  href: null,
-
-  click(e) {
-    const onClick = get(this, 'onClick');
-
-    if (onClick) {
-      e.preventDefault();
-
-      return onClick();
-    }
-  }
 });
 
 export default AlertHeader;
